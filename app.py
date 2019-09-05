8416f9b600c9c248e17d9b23652c56704267a452
= <zainr7989@gmail.com>
2019-09-05 16:58:04 -0700

    Attempt Tenor API integration.

--- app.py
+++ app.py
@@ -4,6 +4,25 @@ import json
 
 app = Flask(__name__)
 
+# lines inspired by https://tenor.com/gifapi/documentation#quickstart-search
+# set the API key and limit for search result
+api_key = "EDBOKVM2ES41"
+limit = 10  # number of search results
+
+# test search term
+search_term = "excited"
+
+r = requests.get(
+    "https://api.tenor.com/v1/search?q=%s&key=%s&limit=%s" %
+    (search_term, api_key, limit))
+
+if r.status_code == 200:
+    # load the GIFs using the urls for smaller GIF sizes
+    top_ten = json.load(r.content)
+    print(top_ten)
+else:
+    top_ten = None
+
 
 @app.route('/')
 def index():
@@ -17,7 +36,8 @@ def index():
 
     # TODO: Get the first 10 results from the search results
 
-    # TODO: Render the 'index.html' template, passing the gifs as a named parameter
+    # TODO: Render the 'index.html' template, passing the gifs
+    # as a named parameter
 
     return render_template("index.html")
 
