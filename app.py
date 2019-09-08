deee8b9f5b2b37bbce103cf302e6bc3f5d269174
Christopher Francisco <franciscochristopher77777@gmail.com>
2019-09-07 17:45:57 -0700

    Add Tenor API integration

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ import requests
 import json
 
 app = Flask(__name__)
-'''
+
 # lines inspired by https://tenor.com/gifapi/documentation#quickstart-search
 # set the API key and limit for search result
 api_key = "EDBOKVM2ES41"
-limit = 10  # number of search results
+limit = 1  # number of search results
 
 # test search term
 search_term = "excited"
@@ -18,34 +18,34 @@ r = requests.get(
 
 if r.status_code == 200:
     # load the GIFs using the urls for smaller GIF sizes
-    top_ten = json.load(r.content)
-    print(top_ten)
+    print(r.content)
+    r.close()
 else:
     top_ten = None
-'''
-
-@app.route('/')
-def index():
-    """Return homepage."""
-    #return "Hello there user!"
-    # TODO: Extract query term from url
-
-    # TODO: Make 'params' dict with query term and API key
-
-    # TODO: Make an API call to Tenor using the 'requests' library
-
-    # TODO: Get the first 10 results from the search results
-
-    # TODO: Render the 'index.html' template, passing the gifs
-    # as a named parameter
-    msg = 'Hello World!'
-    return render_template("index.html", msg = msg)
-
-@app.route('/getGif')
-def get_gif():
-    gif_type = request.args.get('gif_type')
-    return 'The gif type you\'ve requested is ' + gif_type
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# @app.route('/')
+# def index():
+#     """Return homepage."""
+#     return "Hello there user!"
+#     # TODO: Extract query term from url
+
+#     # TODO: Make 'params' dict with query term and API key
+
+#     # TODO: Make an API call to Tenor using the 'requests' library
+
+#     # TODO: Get the first 10 results from the search results
+
+#     # TODO: Render the 'index.html' template, passing the gifs
+#     # as a named parameter
+#     # msg = 'GIF SEARCH!'
+#     # return render_template("index.html", msg = msg)
+
+# @app.route('/getGif')
+# def get_gif():
+#     gif_type = request.args.get('gif_type')
+#     return 'The gif type you\'ve requested is ' + gif_type
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
