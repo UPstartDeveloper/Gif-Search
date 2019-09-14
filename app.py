a8cb92198edd44eccf704cd7bcd8e6e267d00e9f
Christopher Francisco <franciscochristopher77777@gmail.com>
2019-09-13 17:59:15 -0700

    Fix issue in rendering random GIFs.

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ app = Flask(__name__)
 api_key = "EDBOKVM2ES41"  # stores the API key
 limit = 10  # number of search results
 
-
 @app.route('/')
 def index():
     """
@@ -20,35 +19,24 @@ def index():
     """
 
     # Enables random GIFs to show.
-    if requests.args.get('random'):
-        gif_type = ''
-        gif_info = get_gif_info(
-            gif_type, "https://api.tenor.com/v1/random?q=%s&key=%s&limit=%s")
-        return render_template(
-            "index.html", gif_info=gif_info, gif_type=gif_type)
-
-    gif_type = request.args.get('gif_type')
-    if gif_type is None:
-        gif_type = ''
-
-    gif_info = get_gif_info(
-        gif_type, "https://api.tenor.com/v1/random?q=%s&key=%s&limit=%s")
-    return render_template("index.html", gif_info=gif_info, gif_type=gif_type)
+    if request.args.get('random'):
+        gif_type = 'pizza'
+        gif_info = get_gif_info(gif_type, "https://api.tenor.com/v1/random?q=%s&key=%s&limit=%s")
+        return render_template("index.html", gif_info=gif_info, gif_type=gif_type)
 
     # Enables top trends GIFs to show
     if request.args.get('trending'):
         gif_type = ''
         gif_info = get_gif_info(
             gif_type, "https://api.tenor.com/v1/trending?key=%s&limit=%s")
-        return render_template(
-            "index.html", gif_info=gif_info, gif_type=gif_type)
+        return render_template("index.html", gif_info=gif_info, gif_type=gif_type)
 
     gif_type = request.args.get('gif_type')
     if gif_type is None:
         gif_type = ''
 
-    gif_info = get_gif_info(
-            gif_type, "https://api.tenor.com/v1/search?q=%s&key=%s&limit=%s")
+    gif_info = get_gif_info(gif_type, "https://api.tenor.com/v1/search?q=%s&key=%s&limit=%s")
+
     return render_template("index.html", gif_info=gif_info, gif_type=gif_type)
 
 
@@ -63,10 +51,13 @@ def get_gif_info(gif_type, api_link):
         r = requests.get(
             api_link %
             (api_key, limit))
+    elif 'random' in api_link:
+        r = requests.get(api_link %
+            (gif_type, api_key, limit))
     else:
         r = requests.get(
-            api_link %
-            (gif_type, api_key, limit))
+            api_link % (gif_type, api_key, limit))
+
     gif_info = []
     if r.status_code == 200:
         # Use JSON from content to get URLs
