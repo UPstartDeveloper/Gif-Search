9aaa3ed01f4eac0e96898cbfa72a79afbb67f936
= <zainr7989@gmail.com>
2019-09-15 12:46:02 -0700

    Add random list of words to autocomplete user search.

---
+++ autocomplete.js
@@ -0,0 +1,5 @@
+var random_words =
+["books", "love", "family", "beach", "cartoon",
+"movies", "video games", "jelly beans", "sandwich", "string",
+"fruits", "building", "automobile", "ice cream", "mother",
+"journal", "imagination", "automation", "podcast", "dinner"];
