430f62774bce145fcb6128daa2e4412bfbc95e15
= <zainr7989@gmail.com>
2019-09-15 15:13:19 -0700

    Improve syntax for error image element.

--- autocomplete.js
+++ autocomplete.js
@@ -1,21 +1,24 @@
-var random_words =
+var randomWords =
     ["books", "love", "family", "beach", "cartoon",
         "movies", "video games", "jelly beans", "sandwich", "string",
         "fruits", "building", "automobile", "ice cream", "mother",
         "journal", "imagination", "automation", "podcast", "dinner"];
 
-function get_suggestions(searchTerm){
+// Inspired by https://scotch.io/tutorials/how-to-use-the-javascript-fetch-api-to-get-data
+function getSuggestions(searchTerm){
 const fetch = require("node-fetch");
+
+
 fetch("http://suggestqueries.google.com/complete/search?q=" + searchTerm + "&client=safari").
     then((resp) => resp.json()).then(function(data){
- 
+
     for (var index = 0; index < 10; index++){
         var result = JSON.stringify(data[1][index][0])
         result = result.substring(1, result.length - 1)
         console.log(result)
     }
-    
+
 })
 }
 
-get_suggestions('Potato')
+getSuggestions('Potato') // testing getSuggestions
